a6c73d299d5c8c7a715b4976430486a1e22f55d6
Juan Leni <lenijuan@gmail.com>
2019-06-01 14:16:19 +0200

    additional commands + tests

--- README.md
+++ README.md
@@ -2,52 +2,11 @@
 
 [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
 
-This package provides a basic client library to communicate with a Tendermint/Cosmos App running in a Ledger Nano S
+This package provides a basic client library to communicate with a Tendermint/Cosmos App running in a Ledger Nano S/X
 
-There are two running modes:
-
-- *HID*: Direct access via HID. This can be used from a backend, node.js, etc.
-- *U2F*: This allows access to the device from the browser (client side)
+The package is being published in npmjs. It is recommended to use that in order to receive updates and fixes. 
 
 # Testing
 
-Install all dependencies by running
+# Examples
 
-```
-npm install
-``` 
-
-There are a few useful scripts:
-
-- ```npm test```: Will run HID tests
-
-- ```npm browserify```: Will generate js files that are necessary for U2F/browser integration
- 
-- ```npm browserify-test```: Will generate js files that are necessary for browser testing. After executing this script. You can access `tests/browser/index.html` to run browser tests. 
-
-Warning: You need to setup a webserver and point it to index.html. U2F communication requires an https connection.
-
-## How to run browser tests
-
-First you need to deploy the javascript files. Run the following: 
-
-```bash
-npm run browserify
-npm run browserify-test
-```
-
-now go to the test directory and run `caddy`
-```bash
-cd tests/browser
-caddy
-```
-
-you should see something like:
-
-```text
-Activating privacy features... done.
-https://localhost:2020
-WARNING: File descriptor limit 1024 is too low for production servers. At least 8192 is recommended. Fix with `ulimit -n 8192`.
-```
-
-Open your browser at `https://localhost:2020` and you can now run the test
--- src/index.js
+++ src/index.js
@@ -17,6 +17,9 @@
 
 import Transport from '@ledgerhq/hw-transport';
 import { TransportStatusError } from '@ledgerhq/hw-transport';
+import crypto from 'crypto';
+import Ripemd160 from 'ripemd160';
+import bech32 from 'bech32';
 
 const CLA = 0x55;
 const CHUNK_SIZE = 250;
@@ -29,73 +32,53 @@ const INS = {
     GET_ADDR_SECP256K1: 0x04,
 };
 
-function errorMessage(error_code) {
-    switch (error_code) {
-        case 1:
-            return "U2F: Unknown";
-        case 2:
-            return "U2F: Bad request";
-        case 3:
-            return "U2F: Configuration unsupported";
-        case 4:
-            return "U2F: Device Ineligible";
-        case 5:
-            return "U2F: Timeout";
-        case 14:
-            return "Timeout";
-        case 0x9000:
-            return "No errors";
-        case 0x9001:
-            return "Device is busy";
-        case 0x6400:
-            return "Execution Error";
-        case 0x6700:
-            return "Wrong Length";
-        case 0x6982:
-            return "Empty Buffer";
-        case 0x6983:
-            return "Output buffer too small";
-        case 0x6984:
-            return "Data is invalid";
-        case 0x6985:
-            return "Conditions not satisfied";
-        case 0x6986:
-            return "Transaction rejected";
-        case 0x6A80:
-            return "Bad key handle";
-        case 0x6B00:
-            return "Invalid P1/P2";
-        case 0x6D00:
-            return "Instruction not supported";
-        case 0x6E00:
-            return "Cosmos app does not seem to be open";
-        case 0x6F00:
-            return "Unknown error";
-        case 0x6F01:
-            return "Sign/verify error";
-        default:
-            return "Unknown error code";
+const ERROR_DESCRIPTION = {
+    1: "U2F: Unknown",
+    2: "U2F: Bad request",
+    3: "U2F: Configuration unsupported",
+    4: "U2F: Device Ineligible",
+    5: "U2F: Timeout",
+    14: "Timeout",
+    0x9000: "No errors",
+    0x9001: "Device is busy",
+    0x6400: "Execution Error",
+    0x6700: "Wrong Length",
+    0x6982: "Empty Buffer",
+    0x6983: "Output buffer too small",
+    0x6984: "Data is invalid",
+    0x6985: "Conditions not satisfied",
+    0x6986: "Transaction rejected",
+    0x6A80: "Bad key handle",
+    0x6B00: "Invalid P1/P2",
+    0x6D00: "Instruction not supported",
+    0x6E00: "Cosmos app does not seem to be open",
+    0x6F00: "Unknown error",
+    0x6F01: "Sign/verify error",
+};
+
+function errorCodeToString(statusCode) {
+    if (statusCode in ERROR_DESCRIPTION)
+        return ERROR_DESCRIPTION[statusCode];
+    return `Unknown Status Code: ${statusCode}`
+}
+
+export function getBech32FromPK(hrp, pk) {
+    if (pk.length !== 33) {
+        throw new Error("expected compressed public key [31 bytes]");
     }
+    const hash_sha256 = crypto.createHash('sha256').update(pk).digest();
+    const hash_rip = new Ripemd160().update(hash_sha256).digest();
+    return bech32.encode(hrp, bech32.toWords(hash_rip));
 }
 
 function processErrorResponse(response) {
-    let result = {};
-
-    // TODO: Do we still need this?
-    if (typeof response === 'string' || response instanceof String) {
-        // Unfortunately, ledger node implementation returns an string!! :(
-        result["return_code"] = parseInt(response.slice(-4), 16);
-    } else {
-        // Handle U2F communication normally
-        result["return_code"] = response.errorCode;
-    }
-
-    result["error_message"] = errorMessage(result["return_code"]);
-
-    return result;
+    return {
+        return_code: response.statusCode,
+        error_message: errorCodeToString(response.statusCode)
+    };
 }
 
-function serialize_hrp(hrp) {
+function serializeHRP(hrp) {
     if (hrp == null || hrp.length < 3 || hrp.length > 83) {
         throw new Error("Invalid HRP")
     }
@@ -105,7 +88,7 @@ function serialize_hrp(hrp) {
     return buf;
 }
 
-function serialize_path(path) {
+function serializePath(path) {
     if (path == null || path.length < 3) {
         throw new Error("Invalid path.")
     }
@@ -131,6 +114,23 @@ function compressPublicKey(publicKey) {
     const y = publicKey.slice(33, 65);
     const z = new Buffer.from([2 + (y[y.length - 1] & 1)]);
     return Buffer.concat([z, publicKey.slice(1, 33)]);
+}
+
+export function signGetChunks(path, message) {
+    let chunks = [];
+    chunks.push(serializePath(path));
+
+    let buffer = Buffer.from(message);
+
+    for (let i = 0; i < buffer.length; i += CHUNK_SIZE) {
+        let end = i + CHUNK_SIZE;
+        if (i > buffer.length) {
+            end = buffer.length;
+        }
+        chunks.push(buffer.slice(i, end));
+    }
+
+    return chunks;
 }
 
 export default class CosmosApp {
@@ -162,21 +162,118 @@ export default class CosmosApp {
                     const errorCodeData = response.slice(-2);
                     const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
                     return {
+                        return_code: returnCode,
+                        error_message: errorCodeToString(returnCode),
+                        /////
                         test_mode: response[0] !== 0,
                         major: response[1],
                         minor: response[2],
                         patch: response[3],
-                        device_locked: response[4] === 1,
-                        return_code: returnCode,
-                        error_message: errorMessage(returnCode)
+                        device_locked: response[4] === 1
                     };
                 },
                 processErrorResponse
             );
     }
 
+    async appInfo() {
+        return this.transport.send(0xB0, 0x01, 0, 0)
+            .then(
+                (response) => {
+                    const errorCodeData = response.slice(-2);
+                    const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
+
+                    let appName = "err";
+                    let appVersion = "err";
+                    let flagLen = 0;
+                    let flagsValue = 0;
+
+                    if (response[0] !== 1) {
+                        // Ledger responds with format ID 1. There is no spec for any format != 1
+                        result["error_message"] = "response format ID not recognized";
+                        result.return_code = 0x9001;
+                    } else {
+                        const appNameLen = response[1];
+                        appName = response.slice(2, 2 + appNameLen).toString('ascii');
+                        var idx = 2 + appNameLen;
+                        const appVersionLen = response[idx];
+                        idx++;
+                        appVersion = response.slice(idx, idx + appVersionLen).toString('ascii');
+                        idx += appVersionLen;
+                        const appFlagsLen = response[idx];
+                        idx++;
+                        flagLen = appFlagsLen;
+                        flagsValue = response[idx];
+                    }
+
+                    return {
+                        return_code: returnCode,
+                        error_message: errorCodeToString(returnCode),
+                        ////
+                        appName: appName,
+                        appVersion: appVersion,
+                        flagLen: flagLen,
+                        flagsValue: flagsValue,
+                        flag_recovery: (flagsValue & 1) !== 0,
+                        flag_signed_mcu_code: (flagsValue & 2) !== 0,
+                        flag_onboarded: (flagsValue & 4) !== 0,
+                        flag_pin_validated: (flagsValue & 128) !== 0
+                    };
+                },
+                processErrorResponse
+            );
+    }
+
+    async deviceInfo() {
+        return this.transport.send(0xE0, 0x01, 0, 0, Buffer.from([]), [0x9000, 0x6E00])
+            .then(
+                (response) => {
+                    const errorCodeData = response.slice(-2);
+                    const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
+
+                    if (returnCode === 0x6E00) {
+                        return {
+                            return_code: returnCode,
+                            error_message: "This command is only available in the Dashboard",
+                        }
+                    }
+
+                    const targetId = response.slice(0, 4).toString('hex');
+
+                    let pos = 4;
+                    const secureElementVersionLen = response[pos++];
+                    const seVersion = response.slice(pos, pos + secureElementVersionLen).toString();
+                    pos += secureElementVersionLen;
+
+                    const flagsLen = response[pos++];
+                    const flag = response.slice(pos, pos + flagsLen).toString('hex');
+                    pos += flagsLen;
+
+                    const mcuVersionLen = response[pos++];
+                    // Patch issue in mcu version
+                    let tmp = response.slice(pos, pos + mcuVersionLen);
+                    if (tmp[mcuVersionLen - 1] === 0) {
+                        tmp = response.slice(pos, pos + mcuVersionLen - 1);
+                    }
+                    const mcuVersion = tmp.toString();
+
+                    return {
+                        return_code: returnCode,
+                        error_message: errorCodeToString(returnCode),
+                        ////
+                        targetId: targetId,
+                        seVersion: seVersion,
+                        flag: flag,
+                        mcuVersion: mcuVersion,
+                    };
+                }
+                ,
+                processErrorResponse
+            );
+    }
+
     async publicKey(path) {
-        path = serialize_path(path);
+        path = serializePath(path);
         return this.transport.send(CLA, INS.PUBLIC_KEY_SECP256K1, 0, 0, path)
             .then(
                 (response) => {
@@ -187,42 +284,83 @@ export default class CosmosApp {
                         pk: pk,
                         compressed_pk: compressPublicKey(pk),
                         return_code: returnCode,
-                        error_message: errorMessage(returnCode)
+                        error_message: errorCodeToString(returnCode)
                     };
                 },
                 processErrorResponse
             );
     };
 
-    // async getAddress(account, change, addressIndex) {
-    //     const bip44Path = MatrixAIApp.serializeMANBIP44(account, change, addressIndex);
-    //     return this.transport.send(CLA, INS.GETADDR_SECP256K1, 0, 0, bip44Path)
-    //         .then(
-    //             (response) => {
-    //                 const errorCodeData = response.slice(-2);
-    //                 return {
-    //                     pubKey: response.slice(0, 65)
-    //                         .toString('hex'),
-    //                     address: response.slice(65, 97)
-    //                         .toString('ascii'),
-    //                     return_code: errorCodeData[0] * 256 + errorCodeData[1],
-    //                     // TODO: Improve error handle
-    //                     // TODO: Unify error messages
-    //                     error_message: '????',
-    //                 };
-    //             },
-    //             processErrorResponse
-    //         );
-    // }
+    async getAddressAndPubKey(path, hrp) {
+        let data = Buffer.concat([serializeHRP(hrp), serializePath(path)]);
+        return this.transport.send(CLA, INS.GET_ADDR_SECP256K1, 0, 0, data, [0x9000])
+            .then(
+                (response) => {
+                    const errorCodeData = response.slice(-2);
+                    const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
 
-    getBech32FromPK(hrp, pk) {
-        if (pk.length !== 33) {
-            throw new Error("expected compressed public key [31 bytes]");
-        }
-        const tmp = crypto.enc.Hex.parse(pk.toString("hex"));
-        const hash = ripemd160(sha256(tmp)).toString();
-        const address = Buffer.from(hash, "hex");
-        return bech32.encode(hrp, bech32.toWords(address));
+                    let compressedPk = Buffer.from(response.slice(0, 33));
+                    let bech32Address = Buffer.from(response.slice(33, -2)).toString();
+
+                    let pk = Buffer.from(response.slice(0, 65));
+                    return {
+                        bech32_address: bech32Address,
+                        compressed_pk: compressedPk,
+                        return_code: returnCode,
+                        error_message: errorCodeToString(returnCode)
+                    };
+                },
+                processErrorResponse
+            );
     };
 
+    async sign_send_chunk(chunk_idx, chunk_num, chunk) {
+        return this.transport.send(CLA, INS.SIGN_SECP256K1, chunk_idx, chunk_num, chunk, [0x9000, 0x6A80])
+            .then(
+                (response) => {
+                    const errorCodeData = response.slice(-2);
+                    const returnCode = errorCodeData[0] * 256 + errorCodeData[1];
+                    let errorMessage = errorCodeToString(returnCode);
+
+                    if (returnCode === 0x6A80) {
+                        errorMessage = response.slice(0, response.length - 2).toString('ascii');
+                    }
+
+                    let signature = null;
+                    if (response.length > 2) {
+                        signature = response.slice(0, response.length - 2);
+                    }
+
+                    return {
+                        signature: signature,
+                        return_code: returnCode,
+                        error_message: errorMessage
+                    };
+                },
+                processErrorResponse
+            );
+    };
+
+    async sign(path, message) {
+        let chunks = signGetChunks(path, message);
+        return this.sign_send_chunk(1, chunks.length, chunks[0], [0x9000])
+            .then(
+                async (result) => {
+                    for (let i = 1; i < chunks.length; i++) {
+                        result = await this.sign_send_chunk(1 + i, chunks.length, chunks[i]);
+                        if (result.return_code !== 0x9000) {
+                            break;
+                        }
+                    }
+
+                    return {
+                        return_code: result.return_code,
+                        error_message: result.error_message,
+                        /////
+                        signature: result.signature,
+                    };
+                },
+                processErrorResponse
+            );
+    };
 }
