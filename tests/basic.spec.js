f603ecce33399ff87210bdfce6c04edd695af551
Juan Leni <lenijuan@gmail.com>
2019-10-27 15:05:46 +0100

    unit test fix and upgrade dependencies

--- tests/basic.spec.js
+++ tests/basic.spec.js
@@ -1,9 +1,8 @@
-import { getBech32FromPK } from "index.js";
+import CosmosApp from "index.js";
 
 test("check address conversion", async () => {
-  const pkStr =
-    "034fef9cd7c4c63588d3b03feb5281b9d232cba34d6f3d71aee59211ffbfe1fe87";
+  const pkStr = "034fef9cd7c4c63588d3b03feb5281b9d232cba34d6f3d71aee59211ffbfe1fe87";
   const pk = Buffer.from(pkStr, "hex");
-  const addr = getBech32FromPK("cosmos", pk);
+  const addr = CosmosApp.getBech32FromPK("cosmos", pk);
   expect(addr).toEqual("cosmos1w34k53py5v5xyluazqpq65agyajavep2rflq6h");
 });
