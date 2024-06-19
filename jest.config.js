c1a797db9b39d93a90862e5dbe0f759735cd25a5
Carlos Medeiros <carlos.medeiros@zondax.ch>
2024-06-19 11:32:54 +0100

    update deps & yarn options

--- jest.config.js
+++ jest.config.js
@@ -1,7 +1,4 @@
 module.exports = {
-  preset: 'ts-jest',
-  testEnvironment: 'node',
-  transformIgnorePatterns: ['^.+\\.js$'],
-  modulePaths: ['<rootDir>/src', '<rootDir>/tests'],
-  runner: 'jest-serial-runner',
-}
+  preset: "ts-jest",
+  testEnvironment: "node",
+};
